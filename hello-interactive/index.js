17dc7ab495c5416b3f07ff583af332842076cb4b
Heather Johnson <heather@zaalabs.com>
2017-05-26 18:35:01 -0700

    basic hello world button

--- hello-interactive/index.js
+++ hello-interactive/index.js
@@ -1,4 +1,3 @@
-const interactive = require('beam-interactive-node2');
 const ws = require('ws');
 
 interactive.setWebSocket(ws);
@@ -6,6 +5,59 @@ interactive.setWebSocket(ws);
 const client = new interactive.GameClient();
 
 client.open({
-    authToken: '<OAuth Token>',
-    versionId: 1234,
-});
+    authToken: /*'yourauthtoken'*/,
+    versionId: /*yourversionid*/,
+}).then(function () {
+    var size = 10;
+    // Create Button!
+    return client.createControls({
+        // Use default scene
+        sceneID: 'default',
+        controls:
+        [
+          {
+            controlID: "hello_world",
+            kind: 'button',
+            text: "Hello World!",
+            // does not cost sparks as we do not capture transaction on mousedown
+            cost: 0,
+            position: [
+                {
+                    size: 'large',
+                    width: size,
+                    height: size / 2,
+                    x: size,
+                    y: 1,
+                },
+                {
+                    size: 'medium',
+                    width: size,
+                    height: size,
+                    x: size,
+                    y: 1,
+                },
+                {
+                    size: 'small',
+                    width: size,
+                    height: size / 2,
+                    x: size,
+                    y: 1,
+                },
+            ],
+          }
+        ],
+    });
+}).then(function (controls) {
+    controls.forEach(function (control) {
+        // mousedown = button has been pushed
+        control.on('mousedown', function (inputEvent, participant) {
+            // Logs who pushed the button
+            console.log(participant.username + " pushed, " + inputEvent.input.controlID);
+        });
+    });
+    // Controls don't appear unless we tell Interactive that we are ready!
+    client.ready(true);
+});;
+
+// Log when we're connected to interactive
+client.on('open', function () { return console.log('Connected to interactive'); });
