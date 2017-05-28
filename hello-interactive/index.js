ef75986e7b5e57aabfa3a761c2ce32693b0eee32
Heather Johnson <heather@zaalabs.com>
2017-05-27 19:56:12 -0700

    slimmed down basic example, added dotenv process for quick access to environement variables

--- hello-interactive/index.js
+++ hello-interactive/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+const interactive = require('beam-interactive-node2');
 const ws = require('ws');
 
 interactive.setWebSocket(ws);
@@ -5,13 +7,10 @@ interactive.setWebSocket(ws);
 const client = new interactive.GameClient();
 
 client.open({
-    authToken: /*'yourauthtoken'*/,
-    versionId: /*yourversionid*/,
+    authToken: process.env.OAUTH_TOKEN,
+    versionId: process.env.VERSION_ID
 }).then(function () {
-    var size = 10;
-    // Create Button!
-    return client.createControls({
-        // Use default scene
+    client.createControls({
         sceneID: 'default',
         controls:
         [
@@ -19,45 +18,17 @@ client.open({
             controlID: "hello_world",
             kind: 'button',
             text: "Hello World!",
-            // does not cost sparks as we do not capture transaction on mousedown
-            cost: 0,
             position: [
-                {
-                    size: 'large',
-                    width: size,
-                    height: size / 2,
-                    x: size,
-                    y: 1,
-                },
-                {
-                    size: 'medium',
-                    width: size,
-                    height: size,
-                    x: size,
-                    y: 1,
-                },
-                {
-                    size: 'small',
-                    width: size,
-                    height: size / 2,
-                    x: size,
-                    y: 1,
-                },
+                { size: 'large', width: 10, height: 5, x: 1, y: 1 },
+                { size: 'medium', width: 10, height: 5, x: 1, y: 1 },
+                { size: 'small', width: 10, height: 5, x: 1, y: 1 }
             ],
           }
         ],
     });
-}).then(function (controls) {
-    controls.forEach(function (control) {
-        // mousedown = button has been pushed
-        control.on('mousedown', function (inputEvent, participant) {
-            // Logs who pushed the button
-            console.log(participant.username + " pushed, " + inputEvent.input.controlID);
-        });
-    });
-    // Controls don't appear unless we tell Interactive that we are ready!
+
     client.ready(true);
-});;
+});
 
 // Log when we're connected to interactive
 client.on('open', function () { return console.log('Connected to interactive'); });
